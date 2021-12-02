293ae8ec9c5bba334608be08c01a6f95d8504176
Warr1611 <warr7020@gmail.com>
2021-12-02 12:10:10 -0500

    Template and setup for 2021 aoc1-1

---
+++ 2021/aoc1-1.py
@@ -0,0 +1,12 @@
+from .inputs.aoc1_1_input import input
+
+
+def run():
+    # Count the number of times a depth measurement increases from the previous measurement.
+    # (There is no measurement before the first measurement.)
+    for i in range(1, len(input)):
+        pass
+
+
+if __name__ == "__main__":
+    run()
